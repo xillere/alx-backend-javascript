6608450dc98f61f1459e68757638c2cc46133aec
Chideraa <chideraaozobu@gmail.com>
2024-06-18 20:49:05 +0200

    jest est

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,9 +2,8 @@
  * prints in STDOUT the string argument.
  * @param {String} message to display
  */
-
 function displayMessage(message) {
     console.log(message);
-  }
+};
 
 module.exports = displayMessage;
